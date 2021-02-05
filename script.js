cf85b5b1073710dd70c9b5c36c9d8441f71bfdaf
matthewdrake <emasinmatthew@gmail.com>
2021-02-05 16:22:16 -0500

    fiddle with script

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // a small app to print a randomized message each time it is run.
+
+
+// get list of words from text files
+
+const fs = require('fs')
+
+let nouns = fs.readFile('nouns.txt', 'utf-8',  (err, data) => {
+    if (err) throw err;
+
+    return data;
+})
+
+console.log(typeof(nouns));
