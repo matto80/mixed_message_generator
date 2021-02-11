f5d6a29973f66ce465c9ae1e1d2ce366f30ed2a7
Matthew Drake <emasinmatthew@gmail.com>
2021-02-10 23:05:31 -0500

    get it working

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 // a small app to print a randomized message each time it is run.
+//
+// Topic: Electronic bartender conversation.
+//
+// Plan:
+//  End result is to have a mad-libs style generator print randomized messages
+//  with minor interactivity built-in.
+//
+//  e.g. user runs program receives greeting message and a question
+//  user responds to question and receives another message and another question
+//  repeats until user enters 'check please'
 
 
 // get lists of words from text files
@@ -30,11 +40,36 @@ let adjectives = fs.readFileSync('adjectives.txt', 'utf-8',  (err, data) => {
 adjectives = adjectives.split("\n");
 
 
-// print a random string using a word from each list
+// create functions to get a randomized index for each list
 
-let n = Math.floor(Math.random() * nouns.length);
-let v = Math.floor(Math.random() * verbs.length);
-let a = Math.floor(Math.random() * adjectives.length);
+const n = () => Math.floor(Math.random() * nouns.length);
+const v = () => Math.floor(Math.random() * verbs.length);
+const a = () => Math.floor(Math.random() * adjectives.length);
 
-console.log(`${nouns[n]}, ${verbs[v]}, ${adjectives[a]}`);
-//console.log(nouns);
+// make function to captitalize first letter of a string
+
+const capitalize = string => string[0].toUpperCase() + string.slice(1);
+
+// make interactive console user prompt
+
+const bartender = () => {
+  const readline = require("readline");
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  rl.question(`Well howdy there ${adjectives[a()]} ${nouns[n()]}, how are you today you ${adjectives[a()]}-${nouns[n()]}? `, function(state) {
+    rl.question(`${capitalize(adjectives[a()])} ${state} are you? Well, what can I ${verbs[v()]} for you? `, function(drink) {
+      console.log(`All right then ${adjectives[a()]}-${nouns[n()]}, one ${adjectives[a()]} ${drink} coming right up!`);
+      rl.close();
+    });
+  });
+  
+  rl.on("close", function() {
+    console.log(`\n${capitalize(verbs[v()])} ${adjectives[a()]}, you ${adjectives[a()]} ${nouns[n()]}!`);
+    process.exit(0);
+  });
+};
+
+bartender();
